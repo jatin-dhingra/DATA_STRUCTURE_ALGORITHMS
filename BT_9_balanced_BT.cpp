1a203c9c6df1b5e44c17ba7a900d2d16248bb10b
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-01-04 22:43:20 +0530

    Update BT_9_balanced_BT.cpp

--- BT_9_balanced_BT.cpp
+++ BT_9_balanced_BT.cpp
@@ -11,30 +11,29 @@
  */
 class Solution {
 public:
-    bool isBalanced(TreeNode* root) {
-        return dfs(root)!=-1;
-    }
-    int dfs(TreeNode* root)
-    {
-        if(root==NULL)
-        {
-            return 0;
-        }
-        int left=dfs(root->left);
-        if(left==-1)
-        {
-            return -1;
-        }
-        int right=dfs(root->right);
-        if(right==-1)
-        {
-            return -1;
-        }
-        if(abs(left-right)>1)
-        {
-            return -1;
-        }
-        return 1+max(left,right);
-    }
-    
+   bool isBalanced(TreeNode* root) {
+       return check(root)!=-1;
+   }
+   int check(TreeNode *root)
+   {
+       if(root==NULL)
+       {
+           return 0;
+       }
+       int left=check(root->left);
+       if(left==-1)
+       {
+           return -1;
+       }
+       int right=check(root->right);
+       if(right==-1)
+       {
+           return -1;
+       }
+       if(abs(left-right)>1)
+       {
+           return -1;
+       }
+       return max(left,right)+1;
+   }
 };
