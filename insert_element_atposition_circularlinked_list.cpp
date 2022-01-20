f505212216e74418ab8098a85933ed626a510385
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-01-20 23:30:12 +0530

    Create insert_element_atposition_circularlinked_list.cpp

---
+++ insert_element_atposition_circularlinked_list.cpp
@@ -0,0 +1,66 @@
+ #include <bits/stdc++.h>
+ using namespace std;
+ struct Node
+ {
+   int data{};
+   struct Node *next;
+
+ }*head;
+
+ void create(int arr[],int n)
+ {
+   struct Node *t,*last;
+   head=new Node;
+   head->data=arr[0];
+   head->next=head;
+   last=head;
+   for(int i=1;i<n;i++)
+   {
+     t=new Node;
+     t->data=arr[i];
+     t->next=last->next;
+     last->next=t;
+     last=t;
+   }
+ }
+
+ void display(Node *p)
+ {
+   // static int check=0;
+   // if(p!=head|| check==0)
+   // {
+   //   check=1;
+   //   cout<<p->data<<" ";
+   //   display(p->next);
+   // }
+   do{
+     cout<<p->data<<" ";
+     p=p->next;
+   }while(p!=head);
+ }
+
+ void insert(Node *p,int n,int data)
+ {
+   Node *t;
+   Node *x=p;
+   for(int i{};i<n-1;i++){
+     x=x->next;
+   }
+   t=new Node;
+   t->data=data;
+   t->next=x->next;
+   x->next=t;
+
+ }
+
+ int main()
+ {
+   int arr[]={1,2,8,5,6};
+   int n=sizeof(arr)/sizeof(arr[0]);
+   create(arr,n);
+   cout<<endl;
+   display(head);
+   cout<<endl;
+   insert(head,3,199);
+   display(head);
+ }
