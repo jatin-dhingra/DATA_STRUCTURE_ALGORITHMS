bc141029a07db16ea05affb186b1c6bd4126b063
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-03-15 00:29:11 +0530

    Time: 386 ms (50.79%), Space: 69.5 MB (60.80%) - LeetHub

---
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
@@ -0,0 +1,30 @@
+
+class NumStrMinComparator
+{
+public:
+    bool operator() (string &b, string &a) { 
+        if (a.size() != b.size()) return a.size() < b.size();
+        return  a < b;
+    }
+    
+};
+
+class Solution {
+public:
+    
+    string kthLargestNumber(vector<string>& nums, int k) 
+    {
+        
+         priority_queue<string, vector<string>, NumStrMinComparator>h;
+        for(string& i:nums)
+        {
+            h.push(i);
+            if(h.size()>k)
+            {
+                h.pop();
+            }
+        }
+        return h.top();
+        
+    }
+};
