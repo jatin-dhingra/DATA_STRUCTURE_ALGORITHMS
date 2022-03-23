d1892c2c165606016166af509e55dd25223e148e
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-03-23 22:37:33 +0530

    Time: 3 ms (57.32%), Space: 6 MB (48.23%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n)
+    {
+        int count{};
+        while(n)
+        {
+            n&=n-1;
+            count++;
+        }
+        return count;
+        
+        
+    }
+};
