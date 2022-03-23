943681d853cdebbf32e73e2a6f67d3346707955a
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-03-24 01:20:56 +0530

    Added solution - LeetHub

---
+++ Min Subsets with Consecutive Numbers - GFG/min-subsets-with-consecutive-numbers.cpp
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    int numofsubset(int arr[], int n)
+    {
+        // Your code goes here
+        sort(arr,arr+n);
+        int count{1};
+        for(int i{};i<n-1;i++)
+        {
+            if(arr[i]+1!=arr[i+1])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+// { Driver Code Starts.
+// Driven Program
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n+1];
+	    for(int i=0; i<n; i++)
+	        cin>>arr[i];
+        Solution ob;
+	    cout << ob.numofsubset(arr, n) << endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
