662b36d9d49008f6e947e73370b5e5c8ca3f644e
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-04-10 20:24:49 +0530

    Time: 14 ms (5.77%), Space: 8.4 MB (69.92%) - LeetHub

---
+++ 682-baseball-game/682-baseball-game.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int calPoints(vector<string>& ops) 
+    {
+        stack<int>st;
+        int n=ops.size();
+        vector<int>v;
+        for(int i{};i<n;i++)
+        {
+            if(ops[i]=="+"){
+                int num1=st.top(); st.pop();
+                int num2=st.top();
+                st.push(num1);
+                st.push(num1+num2);
+            }
+            
+            else if(ops[i]=="D")
+                st.push(st.top()*2);
+            
+            else if(ops[i]=="C")
+                st.pop();
+            
+            else
+                st.push(stoi(ops[i])); 
+        }
+        int ans{};
+        while(!st.empty())
+        {
+            cout<<st.top()<<" ";
+            ans+=(st.top());
+            st.pop();
+            
+        }
+        return ans;
+    }
+};
