3eda73f182e99073fc7ad0eba890b2e51c5e30df
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-05-28 14:05:52 +0530

    Time: 25 ms (59.12%), Space: 18 MB (65.37%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -3,9 +3,9 @@ public:
     int missingNumber(vector<int>& nums) 
     {
         int n=nums.size();
-        int d=n*(n+1)/2;
-        int x=accumulate(nums.begin(),nums.end(),0);
-        return d-x;
+        int sum=n*(n+1)/2;
+        int arrsum=accumulate(nums.begin(),nums.end(),0);
+        return sum-arrsum;
         
     }
 };
