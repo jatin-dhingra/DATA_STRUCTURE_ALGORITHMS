0c5f870373d3745cfcf364350b57c9e5033bcc68
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-05-28 15:21:18 +0530

    Time: 73 ms (22.66%), Space: 19.9 MB (34.44%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -2,27 +2,31 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& inter) 
     {
-        int n=inter.size();
+        
         vector<vector<int>>ans;
-        if(n==0)
+        if(inter.size()==0)
         {
             return ans;
+            
         }
+        
         sort(inter.begin(),inter.end());
-        vector<int>main=inter[0];
-        for(vector<int>i: inter)
+        vector<int>vec=inter[0];
+        
+        for(vector<int> i:inter)
         {
-            if(i[0]<=main[1])
+            if(i[0]<=vec[1])
             {
-                main[1]=max(main[1],i[1]);
+                vec[1]=max(i[1],vec[1]);
             }
             else
             {
-                ans.push_back(main);
-                main=i;
+                ans.push_back(vec);
+                vec=i;
             }
         }
-        ans.push_back(main);
+        ans.push_back(vec);
         return ans;
     }
+    
 };
