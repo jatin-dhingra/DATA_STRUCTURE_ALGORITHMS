3848e1493c4ab409d4aaee65382478e087f54c5a
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-06-02 04:24:37 +0530

    Time: 28 ms (73.67%), Space: 13.7 MB (26.74%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,22 +1,48 @@
 class Solution {
-    public:
-        bool isBipartite(vector<vector<int>>& graph) 
-        {
-            vector<int> groups(graph.size()); 
-            for(int i = 0; i < graph.size(); ++i)
-                if(groups[i] == 0 && !assign_group(graph, i, 1, groups)) 
-                    return false;
-            return true;
-        }
+public:
     
-    protected:
-        bool assign_group(vector<vector<int>>& graph, int i, int group, vector<int>& groups)
+    bool bfs(int node,vector<vector<int>>&graph,vector<int>&vis)
+    {
+        vis[node]=1;
+        queue<int>q;
+        q.push(node);
+        while(!q.empty())
         {
-            if(groups[i] == -group) return false; 
-            if(groups[i] == group) return true; 
-            groups[i] = group;
-            for(int n : graph[i]) 
-                if(!assign_group(graph, n, -group, groups)) return false; 
-            return true;
+            int val=q.front();
+            q.pop();
+            
+            for(auto it:graph[val])
+            {
+                if(vis[it]==-1)
+                {
+                    vis[it]=1-vis[val];
+                    q.push(it);
+                }
+                else if(vis[it]==vis[val])
+                {
+                    return false;
+                }
+            }
         }
+        return true;
+    }
+    
+    bool isBipartite(vector<vector<int>>& graph)
+    {
+        int n=graph.size();
+        vector<int>vis(n+1,-1);
+        
+        for(int i{};i<n;i++)
+        {
+            if(vis[i]==-1)
+            {
+                if(!bfs(i,graph,vis))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+        
+    }
 };
