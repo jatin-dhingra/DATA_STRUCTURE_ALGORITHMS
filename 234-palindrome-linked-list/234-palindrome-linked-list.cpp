1c8db3bfbd3b286db47f409d08c22bef389bd7bb
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-06-05 01:37:11 +0530

    Time: 270 ms (72.10%), Space: 118 MB (58.49%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,28 +10,50 @@
  */
 class Solution {
 public:
-    bool check(string s)
+    
+    ListNode* reverse(ListNode* head)
     {
-        for(int i{};i<s.size()/2;i++)
+        ListNode* start= NULL;
+        while(head!=NULL)
         {
-            if(s[i]!=s[s.size()-i-1])
+            ListNode* newhead=head->next;
+            head->next=start;
+            start=head;
+            head=newhead;
+        }
+        return start;
+        
+    }
+    
+    bool isPalindrome(ListNode* head) 
+    {
+        if(!head || head->next==NULL)
+        {
+            return true;
+        }
+        
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next!=NULL &&  fast->next->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* midnode=slow;
+        
+        slow->next=reverse(slow->next);
+        slow=slow->next;
+        ListNode* dum=head;
+        while(slow!=NULL)
+        {
+            if(slow->val!=dum->val)
             {
                 return false;
             }
-            
+            dum=dum->next;
+            slow=slow->next;
         }
         return true;
-    }
-    bool isPalindrome(ListNode* head) 
-    {
-        string s{};
-        
-        while(head)
-        {
-            s+=(to_string(head->val));
-            head=head->next;
-        }
-        return check(s);
         
     }
 };
