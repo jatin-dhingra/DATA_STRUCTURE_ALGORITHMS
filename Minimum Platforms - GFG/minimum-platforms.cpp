4ecbaa73d4ffa0e50e57063ae7d5e4aed0545cf4
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-06-09 16:38:48 +0530

    Added solution - LeetHub

---
+++ Minimum Platforms - GFG/minimum-platforms.cpp
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+// Program to find minimum number of platforms
+// required on a railway station
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    int findPlatform(int arr[], int dep[], int n)
+    {
+    	// Your code here
+    	sort(arr,arr+n);
+    	sort(dep,dep+n);
+    	int platform{};
+    	int ans{1};
+    	int ind=1;
+    	int end=0;
+    	int count{1};
+    	while(ind<n && end<n)
+    	{
+    	    if(arr[ind]<=dep[end])
+    	    {
+    	        count++;
+    	    }
+    	    else
+    	    {
+    	        end++;
+    	    }
+    	    ind++;
+    	}
+    	return count;
+    }
+};
+
+
+// { Driver Code Starts.
+// Driver code
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        int dep[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        for(int j=0;j<n;j++){
+            cin>>dep[j];
+        }
+        Solution ob;
+        cout <<ob.findPlatform(arr, dep, n)<<endl;
+    } 
+   return 0;
+}  // } Driver Code Ends
