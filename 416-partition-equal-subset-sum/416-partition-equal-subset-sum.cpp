1bc8b391b8dfcfb996a3654f9c188d90c9131044
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-06-10 18:47:35 +0530

    Time: 160 ms (72.66%), Space: 18 MB (42.58%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -4,10 +4,11 @@ public:
     {
         int n=nums.size();
         int dp[n+1][sum+1];
+        
         for(int i{};i<n+1;i++){
             for(int j{};j<sum+1;j++)
             {
-                if(i==0)
+                if(j>=1 && i==0)
                 {
                     dp[i][j]=0;
                 }
@@ -15,10 +16,7 @@ public:
                 {
                     dp[i][j]=1;
                 }
-                if(i==0 && j==0)
-                {
-                    dp[i][j]=1;
-                }
+                
             }
         }
         for(int i{1};i<n+1;i++)
