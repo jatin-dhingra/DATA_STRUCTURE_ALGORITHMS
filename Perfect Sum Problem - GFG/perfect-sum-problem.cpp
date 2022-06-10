b93593ca5e443f6f37914ff29f54e1445a044970
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-06-10 19:13:41 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -8,29 +8,31 @@ class Solution{
 	public:
 	int perfectSum(int arr[], int n, int sum)
 	{
+        // Your code goes here
+        int mod=1000000007;
         int dp[n+1][sum+1];
-       
-           dp[0][0]=1;
+        dp[0][0]=1;
            for(int j=1;j<sum+1;j++)
            {
                    dp[0][j]=0;
                
            }
-       for(int i=1;i<n+1;i++) 
-       {
-           for(int j=0;j<sum+1;j++)
-           {
-               if(arr[i-1]<=j)
-               {
-                   dp[i][j]=dp[i-1][j]%1000000007 + dp[i-1][j-arr[i-1]]%1000000007;
-               }
-               else{
-                  dp[i][j]=dp[i-1][j]%1000000007; 
-               }
-           }
-       }
-       
-       return dp[n][sum]%1000000007;
+        
+        for(int i{1};i<n+1;i++)
+        {
+            for(int j{0};j<sum+1;j++)
+            {
+                if(arr[i-1]<=j)
+                {
+                    dp[i][j]=((dp[i-1][j-arr[i-1]])%mod+(dp[i-1][j])%mod);
+                }
+                else
+                {
+                    dp[i][j]=(dp[i-1][j])%mod;
+                }
+            }
+        }
+        return (dp[n][sum]%mod);
 	}
 	  
 };
