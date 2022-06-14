349ef78c125958d36a74c86b728afb45c8cc5d4c
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-06-14 09:25:22 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -6,53 +6,62 @@ using namespace std;
 class Solution{
 
   public:
-	int minDifference(int arr[], int n)  { 
+	int minDifference(int arr[], int n)  
+	{
+	    
 	    // Your code goes here
+	    
 	    int sum{};
 	    for(int i{};i<n;i++)
 	    {
 	        sum+=arr[i];
 	    }
 	    int dp[n+1][sum+1];
-	    for (int i = 0; i <= n; i++) 
+	    for(int i{};i<n+1;i++)
 	    {
-		    for (int j = 0; j <= sum; j++) {
-			    if (i == 0)
-				    dp[i][j] = false; 
+	        for(int j{};j<sum+1;j++)
+	        {
+	            if (i == 0)
+				    dp[i][j] = 0; 
 			    if (j == 0)
-				    dp[i][j] = true;
-		    }
-    	} 
+				    dp[i][j] = 1;
+	        }
+	    }
+	    
 	    for(int i{1};i<n+1;i++)
 	    {
 	        for(int j{1};j<sum+1;j++)
 	        {
 	            if(arr[i-1]<=j)
 	            {
-	                dp[i][j]=dp[i-1][j]|| dp[i-1][j-arr[i-1]];
+	                dp[i][j]=dp[i-1][j-arr[i-1]]|| dp[i-1][j];
 	            }
-	            else 
+	            else
 	            {
 	                dp[i][j]=dp[i-1][j];
 	            }
 	        }
 	    }
-	    vector<int>ans;
+	    
+	    vector<int>valid;
 	    for(int i{};i<sum+1;i++)
 	    {
-	        if(dp[n][i]==1)
+	        if(dp[n][i])
 	        {
-	            ans.push_back(i);
+	            valid.push_back(i);
 	        }
 	    }
+	    int ans{INT_MAX};
 	    
-	    int m=INT_MAX;
-	    for(int i:ans)
+	    for(int i:valid)
 	    {
-	        m=min(m,abs(sum-2*i));
+	        ans=min(ans,abs(sum-2*i));
 	    }
-	    return m;
+	    
+	    return ans;
+	    
 	} 
+	
 };
 
 
