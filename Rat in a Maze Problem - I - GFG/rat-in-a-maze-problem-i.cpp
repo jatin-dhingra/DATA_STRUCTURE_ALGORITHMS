b8aeee2d0efbf547a79c6c7d60e09b6d5ba46022
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-06-26 04:05:18 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -10,53 +10,38 @@ using namespace std;
 
 class Solution{
     public:
-    void solve(int i,int j,vector<vector<int>> &m,int n,vector<string>&ans,string move,vector<vector<int>>&vis)
+    void solve(int i,int j,vector<vector<int>> &m,int n,vector<string>&ans,string move,vector<vector<int>> &vis,int di[],int dj[])
     {
         if(i==n-1 && j==n-1)
         {
             ans.push_back(move);
-            return ;
+            return;
         }
-        
-        if(i+1<n && m[i+1][j]==1 && !vis[i+1][j])
+        string dir="DLRU";
+        for(int ind{};ind<4;ind++)
         {
-            vis[i][j]=1;
-            solve(i+1,j,m,n,ans,move+'D',vis);
-            vis[i][j]=0;
-        }
-        
-        if(j-1>=0 && m[i][j-1]==1 && !vis[i][j-1])
-        {
-            vis[i][j]=1;
-            solve(i,j-1,m,n,ans,move+'L',vis);
-            vis[i][j]=0;
-        }
-        
-        if(j+1<n && m[i][j+1]==1 && !vis[i][j+1])
-        {
-            vis[i][j]=1;
-            solve(i,j+1,m,n,ans,move+'R',vis);
-            vis[i][j]=0;
-        }
-        
-        if(i-1>=0 && m[i-1][j]==1 && !vis[i-1][j])
-        {
-            vis[i][j]=1;
-            solve(i-1,j,m,n,ans,move+'U',vis);
-            vis[i][j]=0;
+            int nexti=i+di[ind];
+            int nextj=j+dj[ind];
+            if(nexti>=0 && nextj>=0 && nexti<n && nextj<n && !vis[nexti][nextj] && m[nexti][nextj]==1)
+            {
+                vis[i][j]=1;
+                solve(nexti,nextj,m,n,ans,move+dir[ind],vis,di,dj);
+                vis[i][j]=0;
+            }
         }
     }
     vector<string> findPath(vector<vector<int>> &m, int n) {
         // Your code goes here
-        vector<string>ans;
         vector<vector<int>>vis(n,vector<int>(n,0));
-       
+        vector<string>ans;
+        
+        int di[]={1,0,0,-1};
+        int dj[]={0,-1,1,0};
         if(m[0][0]==1)
         {
-           solve(0,0,m,n,ans,"",vis);
+            solve(0,0,m,n,ans,"",vis,di,dj);
         }
         return ans;
-        
     }
 };
 
