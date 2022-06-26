77f176923d14aeec1183c14181a84bcd8752a2c6
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-06-27 01:37:06 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (70.01%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -2,11 +2,7 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) 
     {
-        if(n<0)
-        {
-            return false;
-        }
-        if(n==0)
+        if(n<=0)
         {
             return false;
         }
