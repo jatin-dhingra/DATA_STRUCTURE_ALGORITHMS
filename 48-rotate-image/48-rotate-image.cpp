8bc676727729829e34d2d9ee3e0a8c0be3b47839
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-06-27 16:43:19 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (33.06%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) 
     {
-        int n=matrix.size();
         
+        int n=matrix.size();
         for(int i{};i<n;i++)
         {
             for(int j{};j<i;j++)
@@ -11,11 +11,11 @@ public:
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
+        
         for(int i{};i<n;i++)
         {
             reverse(matrix[i].begin(),matrix[i].end());
         }
-        
         
     }
 };
