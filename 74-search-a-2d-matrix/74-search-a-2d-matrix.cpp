46ca814611332304084160f039798a231ad395d2
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-06-28 17:41:23 +0530

    Time: 11 ms (18.20%), Space: 9.6 MB (51.59%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -4,18 +4,42 @@ public:
     {
         int m=matrix.size();
         int n=matrix[0].size();
-        for(int i{};i<m;i++)
-        {
+        
+        // brute force;
+//         for(int i{};i<m;i++)
+//         {
             
-            for(int j{};j<n;j++)
+//             for(int j{};j<n;j++)
+//             {
+//                 if(matrix[i][j]==target)
+//                 {
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+        
+        // optimized 
+        int i=0;
+        int j=n-1;
+        
+        while(i>=0 && i<m && j>=0 && j<n)
+        {
+            if(matrix[i][j]==target)
             {
-                if(matrix[i][j]==target)
-                {
-                    return true;
-                }
+                return true;
+            }
+            else if(matrix[i][j]<target)
+            {
+                i++;
+            }
+            else if(matrix[i][j]>target)
+            {
+                j--;
             }
         }
         return false;
+        
         
     }
 };
