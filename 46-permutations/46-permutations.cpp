9dfbcdaf51cdc213f8b808225cbdd45d6302d53e
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-07-12 00:52:19 +0530

    Time: 7 ms (39.58%), Space: 7.7 MB (78.26%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -5,20 +5,22 @@ public:
         if(ind==n)
         {
             ans.push_back(nums);
+            return;
         }
-        for(int i{ind};i<n;i++)
+        
+        for(int i=ind;i<n;i++)
         {
             swap(nums[i],nums[ind]);
             solve(nums,ans,ind+1,n);
             swap(nums[i],nums[ind]);
         }
     }
-    vector<vector<int>> permute(vector<int>& nums)
-    {
-        int n=nums.size();
+    
+    vector<vector<int>> permute(vector<int>& nums) 
+    {   
         vector<vector<int>>ans;
+        int n=nums.size();
         solve(nums,ans,0,n);
         return ans;
-        
     }
 };
