f9c8769bcce85e69fe7732f9023375612414666d
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-07-16 00:43:43 +0530

    Time: 51 ms (94.04%), Space: 76 MB (13.18%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,52 +1,41 @@
 class Solution {
 public:
-    bool subset(vector<int>&nums,int sum)
+    
+    bool solve(int summ,vector<int>&nums,int n,vector<vector<int>> &dp)
     {
-        int n=nums.size();
-        int dp[n+1][sum+1];
-        
-        for(int i{};i<n+1;i++){
-            for(int j{};j<sum+1;j++)
-            {
-                if(j>=1 && i==0)
-                {
-                    dp[i][j]=0;
-                }
-                if(j==0)
-                {
-                    dp[i][j]=1;
-                }
-                
-            }
-        }
-        for(int i{1};i<n+1;i++)
+        if(summ==0)
         {
-            for(int j{1};j<sum+1;j++)
-            {
-                if(nums[i-1]<=j)
-                {
-                    dp[i][j]=dp[i-1][j-nums[i-1]]|| dp[i-1][j];
-                }
-                else
-                {
-                    dp[i][j]=dp[i-1][j];
-                }
-
-            }
+            return true;
         }
-        return dp[n][sum];
+        if(n==0 && summ!=0)
+        {
+            return false;
+        }
+        if(dp[n][summ]!=-1)
+        {
+            return dp[n][summ];
+        }
+        if(nums[n-1]>summ)
+        {
+            return dp[n][summ]=solve(summ,nums,n-1,dp);
+            
+        }
+        return dp[n][summ]=solve(summ-nums[n-1],nums,n-1,dp)|| solve(summ,nums,n-1,dp); 
     }
     
     bool canPartition(vector<int>& nums) 
     {
-        int sum=accumulate(nums.begin(),nums.end(),0);
-        if(sum&1)
+        int n=nums.size();
+        
+        int summ=accumulate(nums.begin(),nums.end(),0);
+        if(summ%2!=0)
         {
             return false;
         }
+        vector<vector<int>>dp(n+1,vector<int>((summ/2)+1,-1));
         
-        return subset(nums,sum/2);
-        
+       
+        return solve(summ/2,nums,n,dp);
         
     }
 };
