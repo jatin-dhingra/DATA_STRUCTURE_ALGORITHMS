27209569ba4910173e31807623657acc1c1da47a
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-07-17 02:05:02 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (70.41%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    void func(vector<vector<int>>&ans,vector<int>&sub,vector<int>&nums,int ind)
+    void solve(vector<vector<int>>&ans,vector<int>&v,vector<int>&nums,int ind)
     {
         if(ind==nums.size())
         {
-            ans.push_back(sub);
+            ans.push_back(v);
             return;
         }
-        
-        sub.push_back(nums[ind]);
-        func(ans,sub,nums,ind+1);
-        sub.pop_back();
-        func(ans,sub,nums,ind+1);
-        
+        v.push_back(nums[ind]);
+        solve(ans,v,nums,ind+1);
+        v.pop_back();
+        solve(ans,v,nums,ind+1);
     }
     vector<vector<int>> subsets(vector<int>& nums) 
     {
-        
         vector<vector<int>>ans;
-        vector<int>sub;
-        func(ans,sub,nums,0);
+        vector<int>v;
+        solve(ans,v,nums,0);
         return ans;
         
     }
