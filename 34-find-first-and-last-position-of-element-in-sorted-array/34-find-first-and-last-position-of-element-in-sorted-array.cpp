7084f90d8f62b8419a813aa041e40a60fee032ab
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-07-25 22:20:14 +0530

    Time: 11 ms (68.40%), Space: 13.7 MB (17.99%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,26 +1,63 @@
 class Solution {
 public:
+    
+    int BS(vector<int>&nums,int target)
+    {
+        int ans=-1;
+        int s=0;
+        int e=nums.size()-1;
+        while(s<=e)
+        {
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target)
+            {
+                ans=mid;
+                e=mid-1;
+            }
+            else if(nums[mid]<target)
+            {
+                s=mid+1;
+            }
+            else
+            {
+                e=mid-1;
+            }
+        }
+        return ans;
+    }
+    
+    int BS2(vector<int>&nums,int target)
+    {
+        int ans=-1;
+        int s=0;
+        int e=nums.size()-1;
+        while(s<=e)
+        {
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target)
+            {
+                ans=mid;
+                s=mid+1;
+            }
+            else if(nums[mid]<target)
+            {
+                s=mid+1;
+            }
+            else
+            {
+                e=mid-1;
+            }
+        }
+        return ans;
+    }
+    
     vector<int> searchRange(vector<int>& nums, int target) 
     {
         vector<int>ans;
-        map<int,vector<int>>m;
-        int  n=nums.size();
-        for(int i{};i<n;i++)
-        {
-            m[nums[i]].push_back(i);
-        }
-        ans=m[target];
-         vector<int>mainans;
-        if(ans.size()==0)
-        {
-             mainans.push_back(-1);
-            mainans.push_back(-1);
-            return mainans;
-        }
-       
-        mainans.push_back(ans[0]);
-        mainans.push_back(ans[ans.size()-1]);
-        return mainans;
-        
+        int leftind=BS(nums,target);
+        int rightind=BS2(nums,target);
+        ans.push_back(leftind);
+        ans.push_back(rightind);
+        return ans;
     }
 };
