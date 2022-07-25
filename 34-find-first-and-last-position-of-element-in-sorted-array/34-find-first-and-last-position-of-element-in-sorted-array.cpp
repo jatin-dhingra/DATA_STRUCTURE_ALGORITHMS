ee6a5124eba7b2106440ff322422aa8e94166246
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-07-25 22:12:17 +0530

    Time: 16 ms (33.20%), Space: 14.9 MB (5.65%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,69 +1,26 @@
 class Solution {
 public:
-    
-    void first(vector<int>&nums,int target,vector<int>&ans)
-    {
-        int n=nums.size();
-        int s=0;
-        int e=n-1;
-        //  0 1  2  3  4  5
-        // 9  10 10 10 10 12
-        int temp{-1};
-        while(s<=e)
-        {
-            int mid=s+(e-s)/2;
-            if(nums[mid]==target)
-            {
-                temp=mid;
-                e=mid-1;
-            }
-            else if(nums[mid]<target)
-            {
-                s=mid+1;
-            }
-            else
-            {
-                e=mid-1;
-            }
-        }
-        ans.push_back(temp);
-        
-    }
-    void last(vector<int>&nums,int target,vector<int>&ans)
-    {
-        int n=nums.size();
-        int s=0;
-        int e=n-1;
-        int temp{-1};
-        while(s<=e)
-        {
-             int mid=s+(e-s)/2;
-             if(nums[mid]==target)
-            {
-                temp=mid;
-                s=mid+1;
-            }
-            else if(nums[mid]<target)
-            {
-                s=mid+1;
-            }
-            else
-            {
-                e=mid-1;
-            }  
-        }
-        
-        ans.push_back(temp);
-    }
-    
-    
     vector<int> searchRange(vector<int>& nums, int target) 
     {
         vector<int>ans;
-        
-        first(nums,target,ans);
-        last(nums,target,ans);
-        return ans;
+        map<int,vector<int>>m;
+        int  n=nums.size();
+        for(int i{};i<n;i++)
+        {
+            m[nums[i]].push_back(i);
+        }
+        ans=m[target];
+         vector<int>mainans;
+        if(ans.size()==0)
+        {
+             mainans.push_back(-1);
+            mainans.push_back(-1);
+            return mainans;
+        }
+       
+        mainans.push_back(ans[0]);
+        mainans.push_back(ans[ans.size()-1]);
+        return mainans;
         
     }
 };
