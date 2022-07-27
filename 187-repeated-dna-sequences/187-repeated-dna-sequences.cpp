8c04317604c7fbef133ed14ccdb47013e87f63a4
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-07-28 01:08:35 +0530

    Time: 251 ms (9.91%), Space: 22.3 MB (53.19%) - LeetHub

---
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) 
+    {
+        vector<string>ans;
+        if(s.size()<=9)
+        {
+            return ans;
+        }
+        map<string,int>m;
+        for(int i{};i<=s.size()-10;i++)
+        {
+            string str=s.substr(i,10);
+            if(m.find(str)==m.end())
+            {
+                m[str]=0;
+            }
+            else if(!m[str])
+            {
+                ans.push_back(str);
+                m[str]=1;
+            }
+        }
+        return ans;
+        
+        
+    }
+};
