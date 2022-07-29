c717f4a6c37301875540b8bea4a59418916ad5d8
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-07-29 22:19:25 +0530

    Time: 74 ms (30.40%), Space: 20.1 MB (15.73%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -22,30 +22,5 @@ public:
         
         return ans;
         
-//         int n=nums.size();
-//         vector<vector<int>>ans;
-//         sort(nums.begin(),nums.end());
-//         if(nums.size()==0)
-//         {
-//             return ans;
-//         }
-//         vector<int>v=nums[0];
-        
-//         for(auto i:nums)
-//         {
-//             if(i[0]<=v[1])
-//             {
-//                 v[1]=max(v[1],i[1]);
-//             }
-//             else
-//             {
-//                 ans.push_back(v);
-//                 v=i;
-//             }
-//         }
-//         ans.push_back(v);
-        
-//         return ans;
-        
     }
 };
