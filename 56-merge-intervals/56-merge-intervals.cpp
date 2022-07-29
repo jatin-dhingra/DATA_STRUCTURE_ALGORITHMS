c4d1390edf632d284c3cb748e1a41fcb976c9c2a
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-07-29 22:19:07 +0530

    Time: 47 ms (76.97%), Space: 20.2 MB (15.73%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& nums) 
+    vector<vector<int>> merge(vector<vector<int>>& inter)
     {
-        int n=nums.size();
+        sort(inter.begin(),inter.end());
         vector<vector<int>>ans;
-        sort(nums.begin(),nums.end());
-        if(nums.size()==0)
-        {
-            return ans;
-        }
-        vector<int>v=nums[0];
+        vector<int>v=inter[0];
         
-        for(auto i:nums)
+        for(auto i:inter)
         {
             if(i[0]<=v[1])
             {
-                v[1]=max(v[1],i[1]);
+                v[1]=max(i[1],v[1]);
             }
             else
             {
@@ -27,6 +22,30 @@ public:
         
         return ans;
         
+//         int n=nums.size();
+//         vector<vector<int>>ans;
+//         sort(nums.begin(),nums.end());
+//         if(nums.size()==0)
+//         {
+//             return ans;
+//         }
+//         vector<int>v=nums[0];
+        
+//         for(auto i:nums)
+//         {
+//             if(i[0]<=v[1])
+//             {
+//                 v[1]=max(v[1],i[1]);
+//             }
+//             else
+//             {
+//                 ans.push_back(v);
+//                 v=i;
+//             }
+//         }
+//         ans.push_back(v);
+        
+//         return ans;
         
     }
 };
