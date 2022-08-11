0240c64ab1952fd2fd314b15742b2037b98c94a8
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-08-12 02:05:26 +0530

    Time: 11 ms (95.32%), Space: 10.4 MB (90.35%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
+   
+    
     int longestCommonSubsequence(string text1, string text2) 
     {
         int n=text1.size();
         int m=text2.size();
         int dp[n+1][m+1];
+       memset(dp,-1,sizeof(dp));
+        
         for(int i{};i<n+1;i++)
         {
             for(int j{};j<m+1;j++)
@@ -12,20 +16,22 @@ public:
                 if(i==0 || j==0)
                 {
                     dp[i][j]=0;
+                    // base condition when n==0 || m==0 i index represent steh text1 lenght and j represent text2 length
                 }
             }
         }
-        
         for(int i{1};i<n+1;i++)
         {
             for(int j{1};j<m+1;j++)
             {
                 if(text1[i-1]==text2[j-1])
                 {
-                    dp[i][j]=dp[i-1][j-1]+1;
+                    // when the both n and m became equal htne simple ignore the rest of the lenght and inc it by 1
+                    dp[i][j]=1+dp[i-1][j-1];
                 }
                 else
                 {
+                    // otherwise simpley go fo n-1 with m chars OR n with m-1 chars
                     dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
