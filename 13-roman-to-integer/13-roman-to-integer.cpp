3cf0bbc506eb7f75c83edcc23d2bfdb91b369671
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-08-15 22:41:36 +0530

    Time: 34 ms (18.25%), Space: 8.5 MB (12.79%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int romanToInt(string s) 
     {
-        int ans{};
         map<char,int>m;
-        m.insert({'I',1});
-        m.insert({'V',5});
-        m.insert({'X',10});
-        m.insert({'L',50});
-        m.insert({'C',100});
-        m.insert({'D',500});
-        m.insert({'M',1000});
+        m['I']=1;
+        m['V']=5;
+        m['X']=10;
+        m['L']=50;
+        m['C']=100;
+        m['D']=500;
+        m['M']=1000;
         
         int n=s.size();
+        int ans{};
         for(int i{};i<n;i++)
         {
             if(m[s[i]]<m[s[i+1]])
