2a783de5df05373987d36785f169823f3941cef3
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-09-09 10:02:06 +0530

    Time: 1543 ms (10.88%), Space: 126.5 MB (37.54%) - LeetHub

---
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    // static bool cmp(vector<int>&a,vector<int>&b)
+    // {
+    //     if(a[0]==b[0])
+    //     {
+    //         return a[1]<b[1];
+    //     }
+    //     return a[0]>b[0];
+    // }
+    int numberOfWeakCharacters(vector<vector<int>>& v) 
+    {
+        int count{};
+        int n=v.size();
+         sort(v.begin(), v.end(), [](vector<int> &a, vector<int> &b){
+            if(a[0]==b[0]){
+                return a[1]<b[1];
+            }
+            return a[0]>b[0];
+        });
+       
+        stack<pair<int,int>>st;
+        st.push({v[0][0],v[0][1]});
+        for(int i{1};i<n;i++)
+        {
+            
+            while(!st.empty() && st.top().second<=v[i][1])
+            {
+                st.pop();
+            }
+            if(!st.empty())
+            {
+                count++;
+            }
+            
+            st.push({v[i][0],v[i][1]});
+        }
+        return count;
+        
+    }
+};
