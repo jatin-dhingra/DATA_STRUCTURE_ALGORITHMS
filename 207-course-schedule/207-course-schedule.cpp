d146757129587302997658f9ef795e0ea746ec52
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-09-17 14:30:28 +0530

    Time: 50 ms (23.48%), Space: 14.4 MB (35.14%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    bool DFS(vector<int>adj[],vector<int>&vis,vector<int>&dfsvis,int node)
+    bool DFS(vector<int>adj[],vector<int>&dfsvis,vector<int>&vis,int node)
     {
         vis[node]=1;
         dfsvis[node]=1;
-        for(auto it:adj[node])
+        for(auto i:adj[node])
         {
-            if(!vis[it])
+            if(!vis[i])
             {
-                if(DFS(adj,vis,dfsvis,it))
+                if(DFS(adj,dfsvis,vis,i))
                 {
                     return true;
                 }
             }
-            else if(dfsvis[it])
+            if(dfsvis[i])
             {
                 return true;
             }
@@ -21,29 +21,28 @@ public:
         dfsvis[node]=0;
         return false;
     }
-    bool canFinish(int numCourses, vector<vector<int>>& pre)
+    bool canFinish(int numCourses, vector<vector<int>>& pre) 
     {
         int n=numCourses;
-
         vector<int>adj[n+1];
+        vector<int>vis(n+1,0);
+        vector<int>dfsvis(n+1,0);
         for(auto i:pre)
         {
             adj[i[1]].push_back(i[0]);
         }
         
-        vector<int>vis(n,0);
-        vector<int>dfsvis(n,0);
-        
         for(int i{};i<n;i++)
         {
             if(!vis[i])
             {
-                if(DFS(adj,vis,dfsvis,i))
+                if(DFS(adj,dfsvis,vis,i))
                 {
                     return false;
                 }
             }
         }
         return true;
+        
     }
 };
