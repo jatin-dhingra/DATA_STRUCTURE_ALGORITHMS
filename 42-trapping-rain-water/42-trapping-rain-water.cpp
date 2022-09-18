23845ab7fa16c9ca64274e88f7bb2e380f1338e3
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-09-18 23:18:36 +0530

    Time: 29 ms (42.13%), Space: 20.5 MB (25.17%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-    int trap(vector<int>& nums) 
+    int trap(vector<int>& height) 
     {
-        int n=nums.size();
-        int left[n],right[n];
-        left[0]=nums[0];
-        right[n-1]=nums[n-1];
+        int n=height.size();
+        vector<int>start(n);
+        vector<int>end(n);
+        start[0]=height[0];
+        end[n-1]=height[n-1];
         
         for(int i{1};i<n;i++)
         {
-            left[i]=max(left[i-1],nums[i]);
+            start[i]=max(start[i-1],height[i]);
         }
         for(int i=n-2;i>=0;i--)
         {
-            right[i]=max(right[i+1],nums[i]);
+            end[i]=max(end[i+1],height[i]);
         }
-        vector<int>change(n);
+        
+        int sum{};
         for(int i{};i<n;i++)
         {
-            change[i]=min(left[i],right[i]);
+            sum+=min(start[i],end[i])-height[i];
         }
-        int ans{};
-        for(int i{};i<n;i++)
-        {
-            ans+=change[i]-nums[i];
-        }
-        cout<<ans;
-        
-        return ans;
-        
+        return sum;
     }
 };
