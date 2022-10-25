cb32234a3d56d0535b9519455253b11b96515d1f
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-10-25 12:12:02 +0530

    Time: 6 ms (74.31%), Space: 11.3 MB (89.94%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -3,10 +3,10 @@ public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) 
     {
         string a{},b{};
-        for(auto i:word1){
+        for(auto &i:word1){
             a+=i;
         }
-        for(auto i:word2){
+        for(auto &i:word2){
             b+=i;
         }
         return a==b;
