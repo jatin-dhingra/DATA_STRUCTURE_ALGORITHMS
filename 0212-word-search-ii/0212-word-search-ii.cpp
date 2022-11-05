f5c8c93150ccd92e369bcb512eac0b2eb26d7bae
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-11-05 23:57:05 +0530

    Time: 1215 ms (52.67%), Space: 14.7 MB (45.04%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+
+    string str;
+    bool vis[13][12];
+
+    unordered_set<string> res;
+    unordered_set<string> vec;
+    unordered_map<string, int> m;
+
+    void rec(int i, int j, vector<vector<char>> &a)
+    {
+        if (vec.find(str)!=vec.end())
+        {
+            string curr;
+
+            for (auto x : str)
+            {
+                curr += x;
+                m[curr]--;
+            }
+
+            vec.erase(curr);
+            res.insert(str);
+        }
+
+        if (i < 0 || j < 0 || i >= a.size() || j >= a[0].size() || vis[i][j] || m[str] == 0)
+        {
+            return;
+        }
+
+        vis[i][j] = 1;
+
+        str += a[i][j];
+
+        rec(i + 1, j, a);
+        rec(i, j + 1, a);
+        rec(i, j - 1, a);
+        rec(i - 1, j, a);
+
+        vis[i][j] = 0;
+
+        str.pop_back();
+    }
+
+    vector<string> findWords(vector<vector<char>>& a, vector<string>& words) 
+    {
+        for (auto x : words)
+        {
+            string curr;
+            vec.insert(x);
+            for (auto y:x)
+            {
+                curr += y;
+                m[curr]++;
+            }
+        }
+        m[""]++;
+        for (int i = 0; i < a.size(); i++)
+        {
+            for (int j = 0; j < a[0].size(); j++)
+            {
+                memset(vis, 0, sizeof(vis));
+                rec(i, j, a);
+            }
+        }
+
+        vector<string> ans;
+
+        for (auto x : res)
+        {
+            ans.push_back(x);
+        }
+
+        return ans;
+    }
+};
