385434ac6ac3bca9f2fab508537f0267b2303b8a
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2023-01-06 01:46:26 +0530

    Time: 655 ms (56.31%), Space: 89.8 MB (28.55%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,12 +1,10 @@
-
-bool  comp(vector<int>&a,vector<int>&b)
-{
-
-    return a[1]<b[1];
-}
 class Solution {
 public:
-    
+     static bool  comp(vector<int>&a,vector<int>&b)
+    {
+        
+        return a[1]<b[1];
+    }
     int findMinArrowShots(vector<vector<int>>& points) 
     {
         int n=points.size();
