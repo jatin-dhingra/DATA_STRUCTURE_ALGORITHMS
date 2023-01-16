974d39dd4c06b98c41580b1ddec82eaf3f3fc04a
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2023-01-17 03:26:54 +0530

    Time: 52 ms (9.89%), Space: 17.6 MB (16.14%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& inter, vector<int>& newinter) 
+    vector<vector<int>> insert(vector<vector<int>>& inter, vector<int>& newinter)
     {
+        int n=inter.size()+1;
+        
+        inter.push_back(newinter);
+        sort(inter.begin(),inter.end());
+        for(auto i:inter)
+        {
+            cout<<i[0]<<" "<<i[1]<<endl;
+        }
+        int i{},j{1};
         vector<vector<int>>ans;
+        ans.push_back(inter[i]);
         
-        int n=inter.size();
-        int i{};
-        
-        while(i<n && inter[i][1]<newinter[0])
+        while(j<n)
         {
-            ans.push_back(inter[i++]);
+            if(ans[i][1]<inter[j][0])
+            {
+                i++;
+                ans.push_back(inter[j]);
+            }
+            else
+            {
+                ans[i][1]=max(ans[i][1],inter[j][1]);
+            }
+            j++;
         }
         
-        vector<int>v;
-        
-        while(i<n && inter[i][0]<=newinter[1])
-        {
-            newinter[0]=min(newinter[0],inter[i][0]);
-            newinter[1]=max(newinter[1],inter[i][1]);
-            i++;
-        }
-        ans.push_back(newinter);
-        
-        while(i<n)
-        {
-            ans.push_back(inter[i++]);
-        }
         return ans;
+        
     }
 };
