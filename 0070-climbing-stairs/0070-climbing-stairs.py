a7d64c9c61b0dab704b135dd8e8105e634a6e87b
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2023-02-09 01:41:47 +0530

    Time: 27 ms (91.48%), Space: 13.8 MB (52.00%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,8 @@
-class Solution {
-public:
-    int climbStairs(int n)
-    {
-        int dp[n+1];
-        dp[0]=1;
-        dp[1]=1;
-        for(int i{2};i<=n;i++)
-        {
-            dp[i]=dp[i-2]+dp[i-1];
-        }
-        return dp[n];
-        
-    }
-};
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        arr=[0]*(n+1)
+        arr[0]=1
+        arr[1]=1
+        for i in range(2,n+1):
+            arr[i]=arr[i-1]+arr[i-2]
+        return arr[n]
