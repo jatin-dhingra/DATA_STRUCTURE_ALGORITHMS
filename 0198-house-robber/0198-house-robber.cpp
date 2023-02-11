c3020dd47f676e84024813fcd043f2adcbf8bba8
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2023-02-12 02:11:04 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (53.77%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,30 +1,44 @@
 class Solution {
 public:
-    int dp[101];
-    int solve(vector<int>&nums,int ind)
-    {
-        if(ind==0)
-        {
-            return nums[0]; 
-        }
-        if(ind<0)
-        {
-            return 0;
-        }
-        if(dp[ind]!=-1)
-        {
-            return dp[ind];
-        }
-        int pick=nums[ind]+solve(nums,ind-2);
-        int notpick=solve(nums,ind-1);
-        return dp[ind]=max(pick,notpick);
-    }
+    // int dp[101];
+    // int solve(vector<int>&nums,int ind)
+    // {
+    //     if(ind<0)
+    //     {
+    //         return 0;
+    //     }
+    //     if(ind==0)
+    //     {
+    //         return nums[ind];
+    //     }
+    //     if(dp[ind]!=-1)
+    //     {
+    //         return dp[ind];
+    //     }
+    //     int take=nums[ind]+solve(nums,ind-2);
+    //     int nottake=solve(nums,ind-1);
+    //     return dp[ind]=max(take,nottake);
+    // }
     int rob(vector<int>& nums) 
     {
         
         int n=nums.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(nums,n-1);
+        
+        int dp[n+1];
+        dp[0]=nums[0];
+        int take{},nottake{};
+        for(int i{1};i<n;i++)
+        {
+            
+            take=nums[i];
+            if(i>1)
+            {
+                take+=dp[i-2];
+            }
+            nottake=dp[i-1];
+            dp[i]=max(take,nottake);
+        }
+        return dp[n-1];
         
     }
 };
