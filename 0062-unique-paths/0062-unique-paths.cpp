787eca3043ce4e840e14ab12530c1b04ee050bd6
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2023-02-22 14:47:38 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (98.17%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int dp[101][101];
-    int solve(int x,int y,int m,int n)
+    int solve(int x,int y)
     {
-        if(x==m-1 && y==n-1)
+        if(x==0 && y==0)
         {
             return 1;
         }
-        if(x>=m || y>=n)
+        if(x<0 || y<0)
         {
             return 0;
         }
@@ -15,14 +15,13 @@ public:
         {
             return dp[x][y];
         }
-        int right=solve(x+1,y,m,n);
-        int down=solve(x,y+1,m,n);
-        return dp[x][y]=right+down;
+        int left=solve(x-1,y);
+        int up=solve(x,y-1);
+        return dp[x][y]=left+up;
     }
     int uniquePaths(int m, int n)
     {
         memset(dp,-1,sizeof(dp));
-        return solve(0,0,m,n);
-        
+        return solve(m-1,n-1);
     }
 };
