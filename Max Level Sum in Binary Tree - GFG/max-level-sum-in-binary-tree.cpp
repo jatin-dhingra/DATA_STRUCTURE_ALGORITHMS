0385b0d2faf720ce55bdb56a577399672841bf4f
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2023-03-07 11:54:52 +0530

    Added solution - LeetHub

---
+++ Max Level Sum in Binary Tree - GFG/max-level-sum-in-binary-tree.cpp
@@ -0,0 +1,161 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+void inOrder(Node *root) {
+    if (!root) return;
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+
+
+// } Driver Code Ends
+/* Tree node structure  used in the program
+ struct Node
+ {
+     int data;
+     Node* left, *right;
+}; */
+
+class Solution{
+  public:
+    /*You are required to complete below method */
+    int maxLevelSum(Node* root) 
+    {
+        
+        // Your code here
+        vector<vector<long long>>vec;
+        queue<Node*>q;
+        q.push(root);
+        if(!root)
+        {
+            return 0;
+        }
+        while(!q.empty())
+        {
+            int s=q.size();
+            vector<long long>v;
+            
+            for(int i{};i<s;i++)
+            {
+                Node* ele=q.front();
+                q.pop();
+                v.push_back(ele->data);
+                if(ele->left)
+                {
+                    q.push(ele->left);
+                }
+                if(ele->right)
+                {
+                    q.push(ele->right);
+                }
+                
+            }
+            vec.push_back(v);
+        }
+        long long sum{LLONG_MIN};
+        for(auto i:vec)
+        {
+            long long sum1{};
+            for(auto j:i)
+            {
+                //cout<<j<<" ";
+                sum1+=j;
+            }
+           // cout<<endl;
+            sum=max(sum,sum1);
+        }
+        return (int)sum;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string treeString;
+        getline(cin, treeString);
+        Solution obj;
+        Node *root = buildTree(treeString);
+        cout << obj.maxLevelSum(root) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
