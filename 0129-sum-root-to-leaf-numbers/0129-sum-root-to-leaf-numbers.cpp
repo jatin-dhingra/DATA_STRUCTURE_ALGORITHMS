e78d7fa8cd947b5ae01f4b355139065ff77ca578
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2023-03-14 13:18:12 +0530

    Time: 7 ms (19.15%), Space: 9.4 MB (18.96%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -11,31 +11,40 @@
  */
 class Solution {
 public:
-    int ans{};
-    void dfs(TreeNode* node,int sum)
-    {
-        if(!node)
-        {
-            return;
-        }
-        sum*=10;
-        sum+=node->val;
-        if(!node->left && !node->right)
-        {
-            ans+=sum;
-            return;
-        }
-        dfs(node->left,sum);
-        dfs(node->right,sum);
-        return;
-    }
     int sumNumbers(TreeNode* root) 
     {
         if(!root)
         {
             return 0;
         }
-        dfs(root,0);
-        return ans;
+        queue<pair<TreeNode*,int>>q;
+        q.push({root,0});
+        
+        int val{};
+        while(!q.empty())
+        {
+            TreeNode* node=q.front().first;
+            int sum=q.front().second;
+            q.pop();
+            sum*=10;
+            sum+=node->val;
+            if(!node->left && !node->right)
+            {
+                val+=sum;
+                
+            }
+            else
+            {
+                if(node->left)
+                {
+                    q.push({node->left,sum});
+                }
+                if(node->right)
+                {
+                    q.push({node->right,sum});
+                }
+            }
+        }
+        return val;
     }
 };
