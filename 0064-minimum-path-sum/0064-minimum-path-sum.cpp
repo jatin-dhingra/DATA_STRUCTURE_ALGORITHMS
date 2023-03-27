07ebc03c169413607cde847ca2acd8e4ccc8b555
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2023-03-27 14:22:27 +0530

    Time: 12 ms (45.82%), Space: 9.9 MB (76.06%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,56 +1,29 @@
 class Solution {
 public:
-//     long long dp[201][201];
-//     long long solve(int i,int j,vector<vector<int>>&grid)
-//     {
-//         if(i==0 && j==0)
-//         {
-//             return grid[i][j];
-//         }
-//         if(i<0 || j<0)
-//         {
-//             return INT_MAX-1;
-//         }
-//         if(dp[i][j]!=-1)
-//         {
-//             return dp[i][j];
-//         }
-            
-//         long long left=grid[i][j]+solve(i-1,j,grid);
-//         long long up=grid[i][j]+solve(i,j-1,grid);
-//         return dp[i][j]=min(left,up);
-//     }
-    int minPathSum(vector<vector<int>>& grid)
+    int dp[201][201];
+    int solve(vector<vector<int>>&grid,int x,int y)
+    {
+        if(x<0 || y<0)
+        {
+            return 1e6;
+        }
+        if(x==0 && y==0)
+        {
+            return grid[0][0];
+        }
+        if(dp[x][y]!=-1)
+        {
+            return  dp[x][y];
+        }
+        int left=grid[x][y]+solve(grid,x-1,y);
+        int up= grid[x][y]+solve(grid,x,y-1);
+        return dp[x][y]=min(left,up);
+    }
+    int minPathSum(vector<vector<int>>& grid) 
     {
         int n=grid.size();
         int m=grid[0].size();
-        
-        vector<int>prev(m,0);
-        for(int i{};i<n;i++)
-        {
-            vector<int>curr(m,0);
-            for(int j{};j<m;j++)
-            {
-                if(i==0 && j==0)
-                {
-                    curr[j]=grid[0][0];
-                }
-                else if(i==0 && j>0)
-                {
-                    curr[j]=curr[j-1]+grid[i][j];
-                }
-                else if(i>0 && j==0)
-                {
-                    curr[j]=prev[j]+grid[i][j];
-                }
-                else
-                {
-                    curr[j]=min(prev[j],curr[j-1])+grid[i][j];
-                }
-            }
-            prev=curr;
-        }
-        return prev[m-1];
-        
+        memset(dp,-1,sizeof(dp));
+        return solve(grid,n-1,m-1);
     }
 };
