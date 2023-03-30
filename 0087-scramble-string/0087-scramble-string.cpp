85050f1c92139b6d0c84332eb05423cdff4de4c1
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2023-03-30 22:36:47 +0530

    Time: 163 ms (50.23%), Space: 32.9 MB (51.70%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    unordered_map<string, int> mp;
+    bool isScramble(string s1, string s2) {
+        if(s1 == s2)
+        {
+            return true;
+        }
+        if(s1.length() <= 1)
+        {
+            return false;
+        }  
+        string key=s1+""+s2;
+        if(mp.count(key))
+        {
+            return mp[key];
+        }
+        int n = s1.size();
+        bool res = false;
+        for(int i = 1; i < n; i++) {
+            bool swap = isScramble(s1.substr(0, i), s2.substr(n-i, i)) && isScramble(s1.substr(i, n-i), s2.substr(0, n-i));
+            bool noswap = isScramble(s1.substr(0, i), s2.substr(0, i)) && isScramble(s1.substr(i, n-i), s2.substr(i, n-i));
+            if(swap || noswap)
+            {
+                res = true;
+                break;
+            }
+        }
+        mp[key] =res;
+        return res;
+    }
+};
