abbb76f5a73a3a78ea5086febe8f8c8f75fdf654
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2023-04-08 14:02:54 +0530

    Time: 9 ms (66.88%), Space: 13.9 MB (93.35%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>&grid,int i,int j,int curr,int color)
+    void dfs(vector<vector<int>>&img,int sr,int sc,int curr,int color)
     {
-        if(i<0 || j<0 || i>=grid.size()|| j>=grid[0].size() || grid[i][j]!=curr )
+        if(sr<0 || sr>=img.size()|| sc<0 || sc>=img[0].size() || img[sr][sc]!=curr)
         {
             return;
         }
-        grid[i][j]=color;
-        
-        dfs(grid,i-1,j,curr,color);
-        dfs(grid,i+1,j,curr,color);
-        dfs(grid,i,j-1,curr,color);
-        dfs(grid,i,j+1,curr,color);
+        img[sr][sc]=color;
+        dfs(img,sr-1,sc,curr,color);
+        dfs(img,sr+1,sc,curr,color);
+        dfs(img,sr,sc+1,curr,color);
+        dfs(img,sr,sc-1,curr,color);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& img, int sr, int sc, int color) 
     {
+        
         int n=img.size();
         int m=img[0].size();
         int curr=img[sr][sc];
-        if(img[sr][sc]!=color)
+        if(curr!=color)
         {
             dfs(img,sr,sc,curr,color);
         }
-        
         return img;
-        
     }
 };
