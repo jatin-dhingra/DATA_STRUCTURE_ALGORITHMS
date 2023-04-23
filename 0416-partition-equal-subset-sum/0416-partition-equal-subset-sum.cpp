beaac29d1cdc3fb720c5b99f49c000be85d42cd7
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2023-04-23 23:57:57 +0530

    Time: 301 ms (63.62%), Space: 95.7 MB (26.40%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,71 +1,38 @@
 class Solution {
 public:
-
-//     bool solve(vector<int>&nums,int ind,int target,vector<vector<int>>&dp)
-//     {
-//         if(target==0)
-//         {
-//             return true;
-//         }
-//         if(ind==0)
-//         {
-//             return nums[0]==target;
-            
-//         }
-//         if(dp[ind][target]!=-1)
-//         {
-//             return dp[ind][target];
-//         }
-//         bool notpick=solve(nums,ind-1,target,dp);
-//         bool pick=false;
-//         if(nums[ind]<=target)
-//         {
-//             pick=solve(nums,ind-1,target-nums[ind],dp);
-//         }
-//         return dp[ind][target]=notpick|pick;
-//     }
-    bool canPartition(vector<int>& nums) {
+    bool solve(vector<int>&nums,int sum,int ind,vector<vector<int>>&dp)
+    {
+        if(sum==0)
+        {
+            return true;
+        }
+        if(ind==0)
+        {
+            return nums[ind]==sum;
+        }
+        if(dp[ind][sum]!=-1)
+        {
+            return dp[ind][sum];
+        }
+        bool notpick=solve(nums,sum,ind-1,dp);
+        bool pick=false;
+        if(nums[ind]<=sum)
+        {
+            pick=solve(nums,sum-nums[ind],ind-1,dp);
+        }
+        return dp[ind][sum]=pick|notpick;
+    }
+    bool canPartition(vector<int>& nums) 
+    {
         int sum=accumulate(nums.begin(),nums.end(),0);
         if(sum&1)
         {
-            return false;
+            return 0;
         }
-        
-        int t=sum/2;
+        sum/=2;
         int n=nums.size();
-       int dp[n+1][t+1];
-       
-       // memset(dp,0,sizeof(dp));
-        for(int i{};i<n+1;i++)
-        {
-            for(int j{};j<t+1;j++)
-            {
-                if(i==0 && j>=1)
-                {
-                    dp[i][j]=0;
-                }
-                if(j==0)
-                {
-                    dp[i][j]=1;
-                }
-                
-            }
-        }
+        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
+        return solve(nums,sum,n-1,dp);
         
-        for(int i{1};i<n+1;i++)
-        {
-            for(int j{1};j<t+1;j++)
-            {
-                if(nums[i-1]<=j)
-                {
-                    dp[i][j]=dp[i-1][j]|| dp[i-1][j-nums[i-1]];
-                }
-                else
-                {
-                    dp[i][j]=dp[i-1][j];
-                }
-            }
-        }
-        return dp[n][t];
     }
 };
